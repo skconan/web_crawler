ccd2ddbc6f5a0e0206a17c97cf0b934a97184602
skconan <supakit.kr@gmail.com>
2018-09-06 21:45:21 +0700

    create constants.py

---
+++ constants.py
@@ -0,0 +1,11 @@
+'''
+    File name: constants.py
+    Author: skconan
+    Date created: 2018/09/06
+    Python Version: 3.6.1
+'''
+
+PATH_PROJECT = r'C:\Users\skconan\Desktop\Workspace\web_crawler' 
+PATH_HTML = PATH_PROJECT+r'\html'
+PATH_ROBOT = PATH_PROJECT+r'\list_robots.txt'
+PATH_SITEMAP = PATH_PROJECT+r'\list_sitemap.txt'
