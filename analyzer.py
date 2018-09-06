1ce42350ab1aa3abf09780743a5b0e0b9c6e33a5
skconan <supakit.kr@gmail.com>
2018-09-06 21:24:32 +0700

    create analyzer class and create get robot method

--- analyzer.py
+++ analyzer.py
@@ -1,25 +1,53 @@
 from urllib.parse import urljoin
+from downloader import Downloader
+from bs4 import BeautifulSoup
+import urllib.request
+import io
+class Analyzer:
+    def __init__(self):
+        pass
 
-def link_parser(raw_html):
-    urls = []
-    pattern_start = '<a href="'
-    pattern_end = '"'
-    index = 0
-    length = len(raw_html)
+    def link_parser(self, raw_html):
+        urls = []
+        pattern_start = '<a href="'
+        pattern_end = '"'
+        index = 0
+        length = len(raw_html)
 
-    while index < length:
-        start = raw_html.find(pattern_start, index)
-        if start > 0:
-            start = start + len(pattern_start)
-            end = raw_html.find(pattern_end, start)
-            link = raw_html[start:end]
-            if len(link) > 0:
-                if link not in urls:
-                    urls.append(link)
-            index = end
-        else:
-            break
-    return urls
+        while index < length:
+            start = raw_html.find(pattern_start, index)
+            if start > 0:
+                start = start + len(pattern_start)
+                end = raw_html.find(pattern_end, start)
+                link = raw_html[start:end]
+                if len(link) > 0:
+                    if link not in urls:
+                        urls.append(link)
+                index = end
+            else:
+                break
+        return urls
 
-def url_normalization(base_url, link):
-    return urljoin(base_url, link)
+    def url_normalization(self, base_url, link):
+        return urljoin(base_url, link)
+
+    def get_robot(self, hostname):
+        try:
+            req = urllib.request.urlopen(hostname+"/robots.txt",data=None)
+            data = io.TextIOWrapper(req, encoding='utf-8')
+            return [True, data.read()]
+        except:
+            return [False, '']
+
+        
+
+if __name__ == '__main__':
+    dl = Downloader()
+    text = dl.get_page('http://www.ku.ac.th/web2012/')
+    print(text)
+    al = Analyzer()
+    urls = al.link_parser(text)
+    print(urls)
+    print(len(urls))
+    print(al.get_robot('http://reddit.com')[1])
+    print(al.get_robot('http://google.co.th')[1])
