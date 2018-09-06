db8b2e3066b9f52dd1237f4987214348d1885a7f
skconan <supakit.kr@gmail.com>
2018-09-06 20:08:53 +0700

    change to class

--- downloader.py
+++ downloader.py
@@ -1,23 +1,31 @@
+'''
+    File name: downloader.py
+    Author: skconan
+    Date created: 2018/09/06
+    Python Version: 3.6.1
+'''
+
 import requests
 
-headers = {
-    'User-Agent': 'My User Agent 1.0',
-    'From': 'supakit.kr@ku.th'
-}
+class Downloader:
+    def __init__(self):
+        self.headers = {
+            'User-Agent': 'Bot by 5810500145 subject 01204453',
+            'From': 'supakit.kr@ku.th'
+        }
 
+    def get_page(self,url):
+        text = ''
+        try:
+            r = requests.get(url, headers=self.headers, timeout=2)
+            text = r.text
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            print('Get Page Error!')
+        return text.lower()
 
-def get_page(url):
-    global headers
-    text = ''
-    try:
-        r = requests.get(url, headers=headers, timeout=2)
-        text = r.text
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        print('Get Page Error!')
-    return text.lower()
-
-
-text = get_page('http://www.ku.ac.th/web2012/')
-print(text)
+if __name__ == '__main__':
+    dl = Downloader()
+    text = dl.get_page('http://www.ku.ac.th/web2012/')
+    print(text)
