3479d1162648e88b90e0a6aff1debe948489940d
skconan <supakit.kr@gmail.com>
2018-09-07 07:32:09 +0700

    create endwith_backslah()

--- src/lib.py
+++ src/lib.py
@@ -11,7 +11,12 @@ import time
 def endwith_slash(text):
     return text + ('/' * (int(text.endswith('/')) ^ 1))
 
+def endwith_backslash(text):
+    return text + ('\\' * (int(text.endswith('\\')) ^ 1))
 
+def slash2backslash(text):
+    return text.replace('/','\\')
+    
 def alert(msg='', delay_time=0, sound=False):
     for i in range(3):
         print('!! ' + msg + ' !!')
